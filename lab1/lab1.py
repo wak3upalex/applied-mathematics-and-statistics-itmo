eeb7cf9c3f1dfee5afc70dd1d41d72c12edb7162
Aleksei Dmitriev <aleshadmi@mail.ru>
2025-09-18 19:55:27 +0300

    fixes about seed to random generation

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -6,18 +6,12 @@ SEED = 123
 def generate_uniform_distribution(low: int, high: int, seed: int | None = None) -> np.ndarray:
     """
     Функция для генерации выборки из равномерного закона распределения на интервале от [0, high)
-
-    Args:
-        low: нижняя граница (TODO сделать возможность прописывать нижнюю границу)
-        high: верхняя граница
-        seed: сид генерации случайных чисел
-
+    :param low:
+    :param high:
+    :param seed:
+    :return: np.ndarray
     """
-    if seed is None:
-        ran_gen = np.random.default_rng()
-    else:
-        ran_gen = np.random.default_rng(seed)
-
+    ran_gen = np.random.default_rng(seed)
     return ran_gen.random(high)
 
 s = generate_uniform_distribution(0, 1, seed=SEED)
