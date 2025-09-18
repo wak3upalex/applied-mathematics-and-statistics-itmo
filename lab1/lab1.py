16f3b5ae657f8bbb7105af2144a2be7fc34f695d
Aleksei Dmitriev <aleshadmi@mail.ru>
2025-09-18 20:43:02 +0300

    Fix to params specifying how many numbers needed to be generated

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -3,18 +3,31 @@ import numpy as np
 SEED = 123
 
 
-def generate_uniform_distribution(low: int, high: int, seed: int | None = None) -> np.ndarray:
+def generate_uniform_distribution(n:int, seed: int | None = None) -> np.ndarray:
     """
     Функция для генерации выборки из равномерного закона распределения на интервале от [0, high)
-    :param low:
-    :param high:
-    :param seed:
-    :return: np.ndarray
-    """
-    ran_gen = np.random.default_rng(seed)
-    return ran_gen.random(high)
 
-s = generate_uniform_distribution(0, 1, seed=SEED)
+    Args:
+        n: кол-во чисел для генерации
+        seed: сид генерации случайных чисел
+    Returns:
+        np.ndarray: массив случайных чисел
+
+
+    """
+    if seed is None:
+        ran_gen = np.random.default_rng()
+    else:
+        ran_gen = np.random.default_rng(seed)
+
+    return ran_gen.random(n)
+
+
+
+
+
+s = generate_uniform_distribution(10, seed=SEED)
+print(len(s))
 print("min>=0?", float(s.min()) >= 0.0)
 print("max<=1?", float(s.max()) <= 1.0)
 print("пример:", s[:5])
