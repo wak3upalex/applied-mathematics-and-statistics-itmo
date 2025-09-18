470546a81e441af81200cc1e4fce098f93124dd8
Aleksei Dmitriev <aleshadmi@mail.ru>
2025-09-18 22:06:08 +0300

    task 2 completed with ecdf

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,11 +1,11 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 SEED = 123
 
 
 def generate_uniform_distribution(n:int, seed: int | None = None) -> np.ndarray:
     """
-    Функция для генерации выборки из равномерного закона распределения на интервале от [0, high)
+    Функция для генерации выборки из равномерного закона распределения на интервале от [0, 1)
 
     Args:
         n: кол-во чисел для генерации
@@ -38,14 +38,32 @@ def ecdf_points(sample: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     y = np.arange(1, n + 1) / n
     return x_sorted, y
 
+def plot_ecdf_vs_theory(sample: np.ndarray, title: str = ""):
+    x_sort_plot, y = ecdf_points(sample)
+    x_plot = np.r_[0.0, x_sort_plot, 1.0]
+    y_plot = np.r_[0.0, y, 1.0]
+    # генеральная F(x)=x на [0,1]
+    grid = np.linspace(0, 1, 500)
 
+    plt.figure()
+    plt.step(x_plot, y_plot, where="post", label="Эмпирическая Fₙ")
+    # генеральная функция распределения
+    plt.plot(grid, grid, label="генеральная F(x)=x")
+    plt.xlim(0, 1)
+    plt.ylim(0, 1)
+    plt.xlabel("x")
+    plt.ylabel("F(x)")
+    plt.title(title or f"ECDF и генеральная функция распределения (n={len(sample)})")
+    plt.legend()
+    plt.show()
 
-s = generate_uniform_distribution(10, seed=SEED)
-print(len(s))
-print("min>=0?", float(s.min()) >= 0.0)
-print("max<=1?", float(s.max()) <= 1.0)
-print("пример:", s[:5])
+for n in [10**1, 10**2, 10**3, 10**4]:
+    s = generate_uniform_distribution(n, seed=SEED)
+    print(len(s))
+    print("min>=0?", float(s.min()) >= 0.0)
+    print("max<=1?", float(s.max()) <= 1.0)
+    print("пример:", s[:5])
 
-x_sort, Fn_x = ecdf_points(s)
-print("Неубывающая", (np.all(np.diff(x_sort) >= 0)))
-print(Fn_x[:5])
+    x_sort, Fn_x = ecdf_points(s)
+    print("Неубывающая", (np.all(np.diff(x_sort) >= 0)))
+    plot_ecdf_vs_theory(s, f"ECDF vs F(x)=x, n={n}")
