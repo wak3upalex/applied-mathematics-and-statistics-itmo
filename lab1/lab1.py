90074cccd6fbd544c2199bf8f8a9e7e4177c90a0
Aleksei Dmitriev <aleshadmi@mail.ru>
2025-09-25 20:47:43 +0300

    Docs Comments tell what have been done

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,9 +1,13 @@
-import numpy as np
+from __future__ import annotations
+
 import matplotlib.pyplot as plt
+import numpy as np
+
+# seed number can be set to any custom one (just to recreate results of work
 SEED = 123
 
 
-def generate_uniform_distribution(n:int, seed: int | None = None) -> np.ndarray:
+def generate_uniform_distribution(n: int, seed: int | None = None) -> np.ndarray:
     """
     Функция для генерации выборки из равномерного закона распределения на интервале от [0, 1)
 
@@ -12,8 +16,6 @@ def generate_uniform_distribution(n:int, seed: int | None = None) -> np.ndarray:
         seed: сид генерации случайных чисел
     Returns:
         np.ndarray: массив случайных чисел
-
-
     """
     if seed is None:
         ran_gen = np.random.default_rng()
@@ -35,10 +37,24 @@ def ecdf_points(sample: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
     x_sorted = np.sort(sample)  # сортируем выборку
 
     n = len(x_sorted)
+    # Ступеньки ЭФР: массив [1/n, 2/n, …, n/n].
+    # Это значения F_n(x(i)).
+
     y = np.arange(1, n + 1) / n
     return x_sorted, y
 
-def plot_ecdf_vs_theory(sample: np.ndarray, title: str = ""):
+
+def plot_ecdf_vs_theory(sample: np.ndarray, title: str = "") -> None:
+    """
+    Функция для рисования ЭФР и теоритической функции F(x) = x на одном графике для наглядности и сравнения
+    Args:
+        sample: выборка
+        title: необязательный заголовок
+
+    Returns:
+        None: выводит график
+    """
+    # получаем точки для ЭФР
     x_sort_plot, y = ecdf_points(sample)
     x_plot = np.r_[0.0, x_sort_plot, 1.0]
     y_plot = np.r_[0.0, y, 1.0]
@@ -46,6 +62,7 @@ def plot_ecdf_vs_theory(sample: np.ndarray, title: str = ""):
     grid = np.linspace(0, 1, 500)
 
     plt.figure()
+    # Рисуем ступенчатую функцию: where="post" делает её правонепрерывной (скачок происходит после точки x_(i)) — так по определению устроена ЭФР.
     plt.step(x_plot, y_plot, where="post", label="Эмпирическая Fₙ")
     # генеральная функция распределения
     plt.plot(grid, grid, label="генеральная F(x)=x")
@@ -57,13 +74,16 @@ def plot_ecdf_vs_theory(sample: np.ndarray, title: str = ""):
     plt.legend()
     plt.show()
 
-for n in [10**1, 10**2, 10**3, 10**4]:
+
+for n in [10 ** 1, 10 ** 2, 10 ** 3, 10 ** 4]:
+    # генерируем выборку
     s = generate_uniform_distribution(n, seed=SEED)
     print(len(s))
     print("min>=0?", float(s.min()) >= 0.0)
     print("max<=1?", float(s.max()) <= 1.0)
     print("пример:", s[:5])
 
+    # Считаем точки для ЭФР по данной выборки
     x_sort, Fn_x = ecdf_points(s)
     print("Неубывающая", (np.all(np.diff(x_sort) >= 0)))
     plot_ecdf_vs_theory(s, f"ECDF vs F(x)=x, n={n}")
